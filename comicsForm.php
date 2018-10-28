0960f314981f9811ff1d5c308103c4ec775701a8
Unknown <delorme.tanguy@gmail.com>
2018-10-28 16:45:16 +0100

    Synchronize file

---
+++ comicsForm.php
@@ -0,0 +1,178 @@
+<html>
+       <head>
+         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+         <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.3.3/vue.js"></script>
+         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+           <title>Titeuf</title>
+       </head>
+       <body>
+             <form action="getForm.php" method="post" id="form">
+                <div class="form-group">
+                    <label>seriesTitre :</label>
+                    <input type="text" placeholder="seriesTitre" class="form-control" name="seriesTitre" v-model="seriesTitre">
+                </div>
+                <div class="form-group">
+                    <label>numero :</label>
+                    <input type="number" placeholder="numero" class="form-control" name="numero" v-model="numero">
+                </div>
+                <div class="form-group">
+                    <label>albumTitre :</label>
+                    <input type="text" placeholder="albumTitre" class="form-control" name="albumTitre" v-model="albumTitre">
+                </div>
+                <div class="form-group">
+                  <label>categorie :</label>
+                  <select v-model="categorie" name="categorie">
+                    <option disabled value="">Choisissez</option>
+                    <option>A</option>
+                    <option>B</option>
+                    <option>C</option>
+                  </select>
+                </div>
+                <div class="form-group">
+                    <label>idDessinateur :</label>
+                    <select v-model="idDessinateur" name="idDessinateur">
+                      <option disabled value="">Choisissez</option>
+                      <option>A</option>
+                      <option>B</option>
+                      <option>C</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label>idScenariste :</label>
+                    <select v-model="idScenariste" name="idScenariste">
+                      <option disabled value="">Choisissez</option>
+                      <option>A</option>
+                      <option>B</option>
+                      <option>C</option>
+                    </select>                </div>
+                <div class="form-group">
+                    <label>idColoriste :</label>
+                    <select v-model="idColoriste" name="idColoriste">
+                      <option disabled value="">Choisissez</option>
+                      <option>A</option>
+                      <option>B</option>
+                      <option>C</option>
+                    </select>                </div>
+                <div class="form-group">
+                    <label>prixVente :</label>
+                    <input type="number" placeholder="prixVente" class="form-control" name="prixVente" v-model="prixVente">
+                </div>
+                <div class="form-group">
+                    <label>editeur :</label>
+                    <select v-model="editeur" name="editeur">
+                      <option disabled value="">Choisissez</option>
+                      <option>A</option>
+                      <option>B</option>
+                      <option>C</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label>editionOriginale :</label>
+                    <input type="checkbox" class="form-control" name="editionOriginale" v-model="editionOriginale">
+                </div>
+                <div class="form-group">
+                    <label>image :</label>
+                    <input type="text" placeholder="image" class="form-control" name="image" v-model="image">
+                </div>
+                <div class="form-group">
+                    <label>descriptif :</label>
+                    <input type="text" placeholder="descriptif" class="form-control" name="descriptif" v-model="descriptif">
+                </div>
+                <div class="form-group">
+                    <label>reference :</label>
+                    <input type="text" placeholder="reference" class="form-control" name="reference" v-model="reference">
+                </div>
+                <div class="form-group">
+                    <label>depotLegal :</label>
+                    <input type="date" placeholder="depotLegal" class="form-control" name="depotLegal" v-model="depotLegal">
+                </div>
+                <div class="form-group">
+                    <label>dateSortie :</label>
+                    <input type="date" placeholder="dateSortie" class="form-control" name="dateSortie" v-model="dateSortie">
+                </div>
+                <div class="form-group">
+                    <label>dateImpression :</label>
+                    <input type="date" placeholder="dateImpression" class="form-control" name="dateImpression" v-model="dateImpression">
+                </div>
+                <div class="form-group">
+                    <label>commentaire :</label>
+                    <input type="text" placeholder="commentaire" class="form-control" name="commentaire" v-model="commentaire">
+                </div>
+                <div class="form-group">
+                    <label>image2 :</label>
+                    <input type="text" placeholder="image2" class="form-control" name="image2" v-model="image2">
+                </div>
+                <div class="form-group">
+                    <label>image3 :</label>
+                    <input type="text" placeholder="image3" class="form-control" name="image3" v-model="image3">
+                </div>
+                <div class="form-group">
+                    <label>image4 :</label>
+                    <input type="text" placeholder="image4" class="form-control" name="image4" v-model="image4">
+                </div>
+                <div class="form-group">
+                    <label>nbPages :</label>
+                    <input type="number" placeholder="nbPages" class="form-control" name="nbPages" v-model="nbPages">
+                </div>
+                <div class="form-group">
+                    <label>matiere :</label>
+                    <input type="text" placeholder="matiere" class="form-control" name="matiere" v-model="matiere">
+                </div>
+                <div class="form-group">
+                    <label>aspect :</label>
+                    <input type="text" placeholder="aspect" class="form-control" name="aspect" v-model="aspect">
+                </div>
+                <div class="form-group">
+                    <label>certificat :</label>
+                    <input type="checkbox" placeholder="certificat" class="form-control" name="certificat" v-model="certificat">
+                </div>
+                <div class="form-group">
+                    <label>numerote :</label>
+                    <input type="checkbox" placeholder="numerote" class="form-control" name="numerote" v-model="numerote">
+                </div>
+                <div class="form-group">
+                    <label>signe :</label>
+                    <input type="checkbox" placeholder="signe" class="form-control" name="signe" v-model="signe">
+                </div>
+                <div class="form-group">
+                    <label>packaging :</label>
+                    <input type="text" placeholder="packaging" class="form-control" name="packaging" v-model="packaging">
+                </div>
+                <div class="form-group">
+                    <label>horsCommerce :</label>
+                    <input type="checkbox" placeholder="horsCommerce" class="form-control" name="horsCommerce" v-model="horsCommerce">
+                </div>
+                <div class="form-group">
+                    <label>hauteur :</label>
+                    <input type="number" placeholder="hauteur" class="form-control" name="hauteur" v-model="hauteur">
+                </div>
+                <div class="form-group">
+                    <label>largeur :</label>
+                    <input type="number" placeholder="largeur" class="form-control" name="largeur" v-model="largeur">
+                </div>
+                <div class="form-group">
+                    <label>profondeur :</label>
+                    <input type="number" placeholder="profondeur" class="form-control" name="profondeur" v-model="profondeur">
+                </div>
+                <div class="form-group">
+                    <label>cote :</label>
+                    <input type="number" placeholder="cote" class="form-control" name="cote" v-model="cote">
+                </div>
+                <div class="form-group">
+                    <label>cotePerso :</label>
+                    <input type="number" placeholder="cotePerso" class="form-control" name="cotePerso" v-model="cotePerso">
+                </div>
+                <div class="form-group">
+                    <label>dateModifCote :</label>
+                    <input type="date" placeholder="dateModifCote" class="form-control" name="dateModifCote" v-model="dateModifCote">
+                </div>
+                <div class="form-group">
+                    <label>origineCote :</label>
+                    <input type="text" placeholder="origineCote" class="form-control" name="origineCote" v-model="origineCote">
+                </div>
+                <button type="submit" class="btn btn-default">Envoyer</button>
+            </form>
+           <script src="comicsForm.js"></script>
+       </body>
+   </html>
---
+++ getForm.php
@@ -0,0 +1,158 @@
+<?php
+try
+{
+ $bdd = new PDO('mysql:host=localhost;dbname=comics;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{
+   die('Erreur : ' . $e->getMessage());
+}
+
+$seriesTitre = $_POST["seriesTitre"];
+$numero = $_POST["numero"];
+$albumTitre = $_POST["albumTitre"];
+if(!isset($_POST["categorie"])){
+  $categorie = "";
+}
+else{
+  $categorie = $_POST["categorie"];
+}
+if(!isset($_POST["idDessinateur"])){
+  $idDessinateur = "";
+}
+else{
+  $idDessinateur = $_POST["idDessinateur"];
+}
+if(!isset($_POST["idScenariste"])){
+  $idScenariste = "";
+}
+else{
+  $idScenariste = $_POST["idScenariste"];
+}
+if(!isset($_POST["idColoriste"])){
+  $idColoriste = "";
+}
+else{
+  $idColoriste = $_POST["idColoriste"];
+}
+$prixVente = $_POST["prixVente"];
+if(!isset($_POST["editeur"])){
+  $editeur = "";
+}
+else{
+  $editeur = $_POST["editeur"];
+}
+if(isset($_POST['editionOriginale'])){
+  $editionOriginale = "true";
+}
+else {
+  $editionOriginale = "false";
+}
+$image = $_POST["image"];
+$descriptif = $_POST["descriptif"];
+$reference = $_POST["reference"];
+if(isset($_POST['depotLegal'])){
+  $depotLegal = NULL;
+}
+else {
+  $depotLegal = $_POST['depotLegal'];
+}
+if(isset($_POST['dateSortie'])){
+  $dateSortie = NULL;
+}
+else {
+  $dateSortie = $_POST['dateSortie'];
+}
+if(isset($_POST['dateImpression'])){
+  $dateImpression = NULL;
+}
+else {
+  $dateImpression = $_POST['dateImpression'];
+}
+$commentaire = $_POST["commentaire"];
+$image2 = $_POST["image2"];
+$image3 = $_POST["image3"];
+$image4 = $_POST["image4"];
+$nbPages = $_POST["nbPages"];
+$matiere = $_POST["matiere"];
+$aspect = $_POST["aspect"];
+if(isset($_POST['certificat'])){
+  $certificat = "true";
+}
+else {
+  $certificat = "false";
+}
+if(isset($_POST['numerote'])){
+  $numerote = "true";
+}
+else {
+  $numerote = "false";
+}
+if(isset($_POST['signe'])){
+  $signe = "true";
+}
+else {
+  $signe = "false";
+}
+$packaging = $_POST["packaging"];
+if(isset($_POST['horsCommerce'])){
+  $horsCommerce = "true";
+}
+else {
+  $horsCommerce = "false";
+}
+$hauteur = $_POST["hauteur"];
+$largeur = $_POST["largeur"];
+$profondeur = $_POST["profondeur"];
+$cote = $_POST["cote"];
+$cotePerso = $_POST["cotePerso"];
+if(isset($_POST['dateImpression'])){
+  $dateModifCote = NULL;
+}
+else {
+  $dateModifCote = $_POST['dateModifCote'];
+}
+$origineCote = $_POST["origineCote"];
+
+
+$reponse =  $bdd->query("SELECT seriesTitre, numero, albumTitre, categorie, idDessinateur,
+idScenariste, idColoriste, prixVente, editeur, editionOriginale, image, descriptif, reference,
+depotLegal, dateSortie, dateImpression, commentaire, image2, image3, image4, nbPages, matiere,
+certificat, aspect, signe, numerote, packaging, hauteur, horsCommerce, largeur, profondeur, cote,
+dateModifCote, cotePerso, origineCote FROM titeuf");
+
+$req = $bdd->prepare('INSERT INTO titeuf(seriesTitre, numero, albumTitre, categorie, idDessinateur,
+idScenariste, idColoriste, prixVente, editeur, editionOriginale, image, descriptif, reference,
+depotLegal, dateSortie, dateImpression, commentaire, image2, image3, image4, nbPages, matiere,
+certificat, aspect, signe, numerote, packaging, hauteur, horsCommerce, largeur, profondeur, cote,
+dateModifCote, cotePerso, origineCote) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)');
+$req->execute([$seriesTitre, $numero, $albumTitre , $categorie, $idDessinateur, $idScenariste,
+$idColoriste, $prixVente, $editeur, $editionOriginale, $image, $descriptif, $reference,
+$depotLegal, $dateSortie, $dateImpression, $commentaire, $image2, $image3, $image4, $nbPages ,
+$matiere, $certificat, $aspect, $signe, $numerote, $packaging, $hauteur, $horsCommerce,
+$largeur, $profondeur, $cote, $dateModifCote, $cotePerso, $origineCote]);
+
+
+
+
+$reponse =  $bdd->query("SELECT seriesTitre, numero, albumTitre, categorie, idDessinateur,
+idScenariste, idColoriste, prixVente, editeur, editionOriginale, image, descriptif, reference,
+depotLegal, dateSortie, dateImpression, commentaire, image2, image3, image4, nbPages, matiere,
+certificat, aspect, signe, numerote, packaging, hauteur, horsCommerce, largeur, profondeur, cote,
+dateModifCote, cotePerso, origineCote FROM comicstable");
+
+$req = $bdd->prepare('INSERT INTO comicstable(seriesTitre, numero, albumTitre, categorie, idDessinateur,
+idScenariste, idColoriste, prixVente, editeur, editionOriginale, image, descriptif, reference,
+depotLegal, dateSortie, dateImpression, commentaire, image2, image3, image4, nbPages, matiere,
+certificat, aspect, signe, numerote, packaging, hauteur, horsCommerce, largeur, profondeur, cote,
+dateModifCote, cotePerso, origineCote)
+VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)');
+$req->execute([$seriesTitre, $numero, $albumTitre , $categorie, $idDessinateur, $idScenariste,
+$idColoriste, $prixVente, $editeur, $editionOriginale, $image, $descriptif, $reference,
+$depotLegal, $dateSortie, $dateImpression, $commentaire, $image2, $image3, $image4, $nbPages ,
+$matiere, $certificat, $aspect, $signe, $numerote, $packaging, $hauteur, $horsCommerce,
+$largeur, $profondeur, $cote, $dateModifCote, $cotePerso, $origineCote]);
+
+header('Location: comicsForm.php');
+  exit();
+?>
